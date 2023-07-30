a36a120e92791a5b99e89b62803917908c228ea0
VirLg <108211942+VirLg@users.noreply.github.com>
2023-07-30 14:47:02 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import Contact from './Contact/Contact';
 import Filter from './Filter/Filter';
-
 import Form from './Form/Form';
 
 const App = function () {
-  const [contacts, setContacts] = useState([]);
+  //*******************use */
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
   const [filter, setFilter] = useState('');
 
+  useEffect(() => {
+    if (localStorage.contacts === []) {
+      setContacts([]);
+    }
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // useEffect(() => {
+  //   console.log('localStorage.contacts', localStorage.contacts);
+  //   if (localStorage.contacts === []) return;
+  //   console.log('first', 111);
+  //   // setContacts(JSON.parse(localStorage.getItem('contacts')));
+  // }, []);
+  //*****************use */
   const addContact = props => {
     const { name, number } = props;
-
     if (contacts) {
       const check = contacts.find(el => el.name === name);
       if (check) return alert('NoNoNo');
-
       setContacts(prev => [{ name, number, id: nanoid() }, ...prev]);
     }
   };
   const filterContact = e => setFilter(e.target.value);
 
-  const deleteContact = id => {
-    setContacts(contacts.filter(el => el.id !== id));
-  };
+  const deleteContact = id => setContacts(contacts.filter(el => el.id !== id));
 
   const visible =
     contacts !== [] && contacts.filter(el => el.name.includes(filter));
